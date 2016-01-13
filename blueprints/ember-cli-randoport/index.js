32d66c607f80c55ac3831846fc9282b590751ca4
Ryan LaBouve <imryanlabouve@gmail.com>
2016-01-13 00:11:46 -0600

    Get all basic functionality working

--- blueprints/ember-cli-randoport/index.js
+++ blueprints/ember-cli-randoport/index.js
@@ -1,6 +1,14 @@
 /*jshint node:true*/
+'use strict';
+
 const chalk = require('chalk');
 const say = chalk.bold.blue;
+const error = chalk.bold.red;
+const path = require('path');
+const fs = require('fs');
+const stripJsonComments = require('strip-json-comments');
+
+const randoPort = require('../../lib/randoPort');
 
 
 
@@ -11,7 +19,26 @@ module.exports = {
 
   afterInstall: function(options) {
     console.log(say('Randomizing port!'));
-    console.log(__dirname);
+    // const dotEmberCliPath = path.join(__dirname, '..', '..', '.ember-cli'); // <-- nope
+    const dotEmberCliPath = path.join(this.project.root, '.ember-cli');
 
+    let currentDotEmberCli = {};
+    try {
+      // see if file exists
+      currentDotEmberCli = JSON.parse(stripJsonComments(fs.readFileSync(dotEmberCliPath, 'utf-8')));
+    } catch (err) {
+      console.log(error("[ember-cli-randoport] Could not read your `.ember-cli`"));
+      console.log(say("[ember-cli-randoport] Initializing `.ember-cli`"));
+    }
+
+    console.log('old', currentDotEmberCli);
+    let newDotEmberCli = randoPort(currentDotEmberCli);
+    try {
+      fs.writeFileSync(dotEmberCliPath, JSON.stringify(newDotEmberCli));
+    } catch (err) {
+      console.log(error("[ember-cli-randoport] Error randoport-ing"));
+    }
+
+    console.log('new', newDotEmberCli);
   }
 };
